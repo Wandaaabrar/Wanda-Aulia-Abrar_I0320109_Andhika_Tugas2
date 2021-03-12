964889863b53b33fb160198310bf6cb3e9971db2
Wandaaabrar <80144837+Wandaaabrar@users.noreply.github.com>
2021-03-12 23:49:53 +0700

    Tugas 2 Praktek Prokom

---
+++ I0320109_Soal2_Tugas2.py
@@ -0,0 +1,34 @@
+nama = "Wanda Aulia Abrar"
+nama_panggilan = "Wanda"
+tempat_lahir = "Magelang"
+tanggal_lahir = 6
+bulan_lahir = "Juli"
+tahun_lahir = 2002
+alamat = "Desa Muneng, Kecamatan Pakis, Kabupaten Magelang, Jawa Tengah"
+umur = 17
+anak_ke = 1
+jumlah_saudara = 1
+agama = "Islam"
+golongan_darah = "B"
+sma = "SMA IT Ihsanul Fikri"
+universitas = "UNS"
+jurusan = "Teknik Industri"
+hobi = "membaca, menulis,dan menggambar"
+makanan_favorit = "nasi goreng"
+minuman_favorit = "air putih, kopi, dan susu"
+
+print("Biodata")
+print("Nama saya ", nama, "atau biasa dipanggil ", nama_panggilan)
+print("Saya lahir di ", tempat_lahir, "pada tanggal ", tanggal_lahir, "di bulan ", bulan_lahir, "tahun ", tahun_lahir)
+print("Sekarang saya berumur ", umur, "tahun ")
+print("Saya bertempat tinggal di ", alamat)
+print("Agama saya adalah ", agama)
+print("Saya adalah anak ke", anak_ke)
+print("Saya memiliki ", jumlah_saudara, "saudara ")
+print("Golongan darah saya adalah ", golongan_darah)
+print("Saya adalah alumni dari ", sma)
+print("Sekarang saya kuliah di ", universitas, "jurusan ", jurusan)
+print("Saya memiliki hobi ", hobi)
+print("Makanan favorit saya adalah ", makanan_favorit)
+print("Untuk minuman favorit saya memilih ", minuman_favorit)
+
---
+++ I0320109_exercise2.2.py
@@ -0,0 +1,30 @@
+#proses pemasukan data ke dalam variabel
+nama = "John Doe"
+#proses mencetak variabel
+print(nama)
+
+#nilai dan tipe data dalam variabel dapat diubah
+umur = 20
+print(umur)
+type(umur)
+umur = "dua puluh satu"
+print(umur)
+type(umur)
+
+namaDepan = "Budi"
+namaBelakang = "Susanto"
+nama = namaDepan + " " + namaBelakang
+umur = 22
+hobi = "Berenang"
+print("Biodata\n", nama, "\n", umur, "\n", hobi)
+
+#contoh variabel lainnya
+inivariable = "Halo"
+ini_juga_variable = "Hai"
+_inivariablejuga = "Hi"
+inivariable222 = "Bye"
+
+panjang = 10
+lebar = 5
+luas = panjang * lebar
+print(luas)
